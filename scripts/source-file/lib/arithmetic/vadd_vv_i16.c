eacdb42aa584d3e36b8cf7524db7e5d1b76c5568
cfang <cathy.fang@analog.com>
2021-01-05 11:28:18 +0800

    Add three c file as demo

---
+++ scripts/source-file/lib/arithmetic/vadd_vv_i16.c
@@ -0,0 +1,39 @@
+#include "rivai_rugrats.h"
+#include "rivai_bare.h"
+
+int main() {
+    unsigned long start = 0, stop = 0;
+    int i,j;
+    int error = 0;
+
+    int16x32_t a = ;
+    int16x32_t b = ;
+    int element_num = 32;
+    int16x32_t result = {0};
+    int16x32_t exp_result = {0};
+
+    start=cycles();
+    result = vadd_vv_i16(a,b);
+    stop=cycles();
+
+    printf("cycles \t= stop-start \t= %u - %u = %u\n",stop,start,stop-start);
+    printf("result={");
+    for(i=0;i<element_num;i++) {
+        if(i==element_num-1)
+            printf("%d}\n",result[i]);
+        else
+            printf("%d,",result[i]);
+
+        if(exp_result[i] != result[i]) error = 1;
+    }
+
+    if(error)
+        printf("TEST FAILED!\n");
+    else
+        printf("TEST PASSED!\n");
+
+    // The //while(1) here is a workaround solution to resolve an issue in simulator.
+    // Once the simulator issue is fixed, the //while(1) will be removed in the c src.
+    //while(1);
+    return 0;
+}
---
+++ scripts/source-file/lib/store/vsstcb1_v_i16.c
@@ -0,0 +1,36 @@
+#include "rivai_rugrats.h"
+#include "rivai_bare.h"
+
+int main() {
+    unsigned long start = 0, stop = 0;
+    int i,j;
+    int error = 0;
+
+    int combo_num = 1;
+    int element_num = 32;
+    void result = {0};
+    void exp_result = {0};
+
+    vsstcb1_v_i16(base,index,value);
+
+    printf("cycles \t= stop-start \t= %u - %u = %u\n",stop,start,stop-start);
+    printf("result={");
+    for(i=0;i<element_num;i++) {
+        if(i==element_num-1)
+            printf("%d}\n",result[i]);
+        else
+            printf("%d,",result[i]);
+
+        if(exp_result[i] != result[i]) error = 1;
+    }
+
+    if(error)
+        printf("TEST FAILED!\n");
+    else
+        printf("TEST PASSED!\n");
+
+    // The //while(1) here is a workaround solution to resolve an issue in simulator.
+    // Once the simulator issue is fixed, the //while(1) will be removed in the c src.
+    //while(1);
+    return 0;
+}
